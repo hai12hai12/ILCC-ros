3602841de8c6471bb44151dec6af0a82500b401a
hai12hai12 <91259401+hai12hai12@users.noreply.github.com>
2024-01-28 21:15:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['ILCC'],
+    packages=['ilcc-ros'],
     package_dir={'': 'src'},
 )
 
